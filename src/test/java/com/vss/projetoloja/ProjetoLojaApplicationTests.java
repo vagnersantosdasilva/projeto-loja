01a9146a1813f6ba8daed7eb8f3a9fddf1d34bd6
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2021-06-14 21:56:07 -0300

    tarefa 1 : conexao com banco configurada

---
+++ src/test/java/com/vss/projetoloja/ProjetoLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vss.projetoloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
